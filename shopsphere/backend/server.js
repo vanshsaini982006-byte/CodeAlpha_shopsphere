140130f32375159c3f1ce63fc49edf81b4602d25
Personal-Portfolio <vanshsaini982006@gmail.com>
2026-08-08 23:30:40 +0530

    Fixed app initialization in server.js

--- shopsphere/backend/server.js
+++ shopsphere/backend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const app =express();
 app.use(cors());
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
